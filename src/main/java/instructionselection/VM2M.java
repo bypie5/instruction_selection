f305c13a82993809cf723b32c4c39a7355b5f48d
Brandon Yi <bypie966@gmail.com>
2020-03-13 17:02:51 -0700

    Set up interfaces for translation

--- src/main/java/instructionselection/VM2M.java
+++ src/main/java/instructionselection/VM2M.java
@@ -1,9 +1,21 @@
 package instructionselection;
 
+import cs132.vapor.ast.VaporProgram;
 import cs132.vapor.parser.VaporParser;
 
 public class VM2M {
     public static void main(String[] args) {
-        VaporParser vaporParser;
+        instructionSelection();
+    }
+
+    public static void instructionSelection() {
+        try {
+            VaporProgram vaporProgram = ParseVapor.parseVapor(System.in, System.err);
+
+
+        } catch (Exception e) {
+            System.out.println(e.toString());
+            e.printStackTrace(System.out);
+        }
     }
 }
