9d513467be1758714666f565e4477f4a22a6759f
Brandon Yi <bypie966@gmail.com>
2020-03-13 20:39:38 -0700

    Label inserstion

--- src/main/java/instructionselection/VaporVisitor.java
+++ src/main/java/instructionselection/VaporVisitor.java
@@ -10,6 +10,7 @@ public class VaporVisitor <E extends Throwable> extends Visitor<E> {
 
     VFunction currFunc;
     List<String> buffer;
+    //int lineCount;
 
     /*
         Interface functions
@@ -18,9 +19,17 @@ public class VaporVisitor <E extends Throwable> extends Visitor<E> {
     public void setData(VFunction currFunc) {
         this.currFunc = currFunc;
         buffer = new ArrayList<>();
+
+        for (int i = 0; i <= currFunc.body.length + currFunc.labels.length; i++) {
+            buffer.add("");
+        }
+
+        //lineCount = 0;
     }
 
     public void printBuffer() {
+        insertLabels();
+
         for (String line : buffer) {
             System.out.println(line);
         }
@@ -30,8 +39,18 @@ public class VaporVisitor <E extends Throwable> extends Visitor<E> {
         Helper functions
     */
 
-    void addLine(String line) {
-        buffer.add(line);
+    int getRelativePos(int sourcePos) {
+        return (sourcePos - currFunc.sourcePos.line) - 1;
+    }
+
+    void insertLabels() {
+        for (int i = 0; i < currFunc.labels.length; i++) {
+            buffer.set(getRelativePos(currFunc.labels[i].sourcePos.line), currFunc.labels[i].ident + ":");
+        }
+    }
+
+    void addLine(int pos, String line) {
+        buffer.set(pos,line);
     }
 
     /*
@@ -40,6 +59,7 @@ public class VaporVisitor <E extends Throwable> extends Visitor<E> {
 
     public void visit(VAssign a) throws E {
         String currLine = "";
+        int relPos = getRelativePos(a.sourcePos.line);
 
         String destReg = a.dest.toString();
 
@@ -49,11 +69,12 @@ public class VaporVisitor <E extends Throwable> extends Visitor<E> {
             currLine += "li " + destReg + " " + a.source.toString();
         }
 
-        addLine(currLine);
+        addLine(relPos, currLine);
     }
 
     public void visit(VCall c) throws E {
         String currLine = "";
+        int relPos = getRelativePos(c.sourcePos.line);
 
         if (c.addr instanceof VAddr.Var) {
             currLine += "jalr " + c.addr.toString();
@@ -61,25 +82,78 @@ public class VaporVisitor <E extends Throwable> extends Visitor<E> {
 
         }
 
-        addLine(currLine);
+        addLine(relPos, currLine);
     }
 
     public void visit(VBuiltIn c) throws E {
+        String currLine = "";
+        int relPos = getRelativePos(c.sourcePos.line);
 
+        switch (c.op.name) {
+            case "Add":
+                currLine += "addu " + c.dest.toString() + " " + c.args[0].toString() + " " + c.args[1].toString();
+                break;
+            case "Sub":
+                currLine += "subu " + c.dest.toString() + " " + c.args[0].toString() + " " + c.args[1].toString();
+                break;
+            case "LtS":
+                currLine += "slti " + c.dest.toString() + " " + c.args[0].toString() + " " + c.args[1].toString();
+                break;
+            case "MulS":
+                currLine += "mul " + c.dest.toString() + " " + c.args[0].toString() + " " + c.args[1].toString();
+                break;
+            case "PrintIntS":
+                currLine += "move $a0 " + c.args[0].toString() + "\n";
+                currLine += "jal _print";
+                break;
+            case "HeapAllocZ":
+                currLine += "li $a0 " + c.args[0].toString() + "\n";
+                currLine += "jal _heapAlloc";
+                break;
+            case "Error":
+                currLine += "la $a0 _str0\n";
+                currLine += "j _error";
+                break;
+            default:
+                currLine += "(" + c.op.name + ") UNDEFINED";
+                break;
+        }
+
+        addLine(relPos, currLine);
     }
 
     public void visit(VMemWrite w) throws E {
+        String currLine = "";
+        int relPos = getRelativePos(w.sourcePos.line);
+
+        addLine(relPos, currLine);
     }
 
     public void visit(VMemRead r) throws E {
+        String currLine = "";
+        int relPos = getRelativePos(r.sourcePos.line);
+
+        addLine(relPos, currLine);
     }
 
     public void visit(VBranch b) throws E {
+        String currLine = "";
+        int relPos = getRelativePos(b.sourcePos.line);
+
+        addLine(relPos, currLine);
     }
 
     public void visit(VGoto g) throws E {
+        String currLine = "";
+        int relPos = getRelativePos(g.sourcePos.line);
+
+        addLine(relPos, currLine);
     }
 
     public void visit(VReturn r) throws E {
+        String currLine = "";
+        int relPos = getRelativePos(r.sourcePos.line);
+
+        addLine(relPos, currLine);
     }
 }
